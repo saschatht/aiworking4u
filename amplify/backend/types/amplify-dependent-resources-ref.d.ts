e2f8d00c162a63e74663551869f1485cbc481e1b
Alejandro Martinez <saschatht@hotmail.com>
2024-06-28 14:07:07 -0600

    created dev branch

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,23 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "BedrockConverse": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "aiworking4ubackendutils": {
+      "Arn": "string"
+    },
+    "aiworking4uboto3": {
+      "Arn": "string"
+    },
+    "aiworking4ugooglesearch": {
+      "Arn": "string"
+    },
+    "aiworking4uhtmlutils": {
+      "Arn": "string"
+    }
+  }
+}
